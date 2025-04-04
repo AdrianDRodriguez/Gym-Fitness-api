1ba3c2339a21f7f8a40a8e0859def72717984fae
Adrian Rodriguez <ar9897.r@gmail.com>
2025-04-03 20:11:03 -0500

    Create, update and destroy actions

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -8,4 +8,33 @@ class ExercisesController < ApplicationController
     @exercise = Exercise.find_by(id: params[:id])
     render :show
   end
+
+  def create
+    @exercise = Exercise.new(
+      name: params[:name],
+      description: params[:description],
+      image_url: params[:image_url],
+      video_url: params[:video_url]
+    )
+    @exercise.save
+    render :show
+  end
+
+  def update
+    @exercise = Exercise.find_by(id: params[:id])
+    @exercise.update(
+      name: params[:name] || @exercise.name,
+      description: params[:description] || @exercise.description,
+      image_url: params[:image_url] || @exercise.image_url,
+      video_url: params[:video_url] || @exercise.video_url
+    )
+    @exercise.save
+    render :show
+  end
+
+  def destroy
+    @exercise = Exercise.find_by(id: params[:id])
+    @exercise.destroy
+    render json: { message: "Exercise eliminated" }
+  end
 end
